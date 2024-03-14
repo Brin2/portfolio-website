9c7a687f9abeef4049c1e400ec99cf8a1175091a
Brian Nyamongo <140794930+Brin2@users.noreply.github.com>
2024-03-14 12:10:06 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+    const body = document.body;
+
+            // Toggle between light and dark mode classes
+            body.classList.toggle("light-mode");
+            body.classList.toggle("dark-mode");
+        }
+
+
+
